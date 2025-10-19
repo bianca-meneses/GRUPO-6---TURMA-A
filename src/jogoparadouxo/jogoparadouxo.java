0a985775f1a9cf4e4c3d0eb7d7bb740492ede6ed
bibs <seuemail@example.comgit git config --global user.name BIANCAgit config --global user.email bianca.meneses@outlook.com>
2025-10-19 18:40:44 -0300

    Primeiro commit do projeto

---
+++ src/jogoparadouxo/jogoparadouxo.java
@@ -0,0 +1,17 @@
+
+package jogoparadouxo;
+
+/*
+ * uhuuu vamos começar a codar!!!!!!!! *-*
+ */
+
+
+public class jogoparadouxo {
+
+    public static void main(String[] args) {
+      
+        System.out.println("Isso é um teste!!!");
+        
+    }
+    
+}
